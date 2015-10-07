959b716ea5022d1656c681f8ec88af26281d8fda
andela-vdugeri <verem.dugeri@andela.co>
2015-10-07 22:46:02 +0100

    Add custome exception class

---
+++ src/Exceptions/RecordNotFoundException.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andela
+ * Date: 10/7/15
+ * Time: 5:50 PM
+ */
+
+namespace Verem\Emoji\Api\Exceptions;
+
+
+use Exception;
+
+class RecordNotFoundException extends Exception {
+
+
+	private $message;
+	public function __construct($message)
+	{
+		parent::__construct($message);
+	}
+
+	public function getErrorMessage()
+	{
+		return $this->message;
+	}
+}
